013ccc31733550b954b33430c035baee9d64982b
tkervancioglu98 <tkervancioglu@bitbucket.org>
2022-12-26 13:23:20 +0300

    qwerty keyboard with error

--- Assets/OpenPanel.cs
+++ Assets/OpenPanel.cs
@@ -14,6 +14,28 @@ public class OpenPanel : MonoBehaviour
             LetterA.SetActive(!isActive);
             LetterB.SetActive(!isActive);
             LetterC.SetActive(!isActive);
+            LetterD.SetActive(!isActive);
+            LetterE.SetActive(!isActive);
+            LetterF.SetActive(!isActive);
+            LetterG.SetActive(!isActive);
+            LetterH.SetActive(!isActive);
+            LetterJ.SetActive(!isActive);
+            LetterK.SetActive(!isActive);
+            LetterL.SetActive(!isActive);
+            LetterM.SetActive(!isActive);
+            LetterN.SetActive(!isActive);
+            LetterO.SetActive(!isActive);
+            LetterP.SetActive(!isActive);
+            LetterQ.SetActive(!isActive);
+            LetterR.SetActive(!isActive);
+            LetterS.SetActive(!isActive);
+            LetterT.SetActive(!isActive);
+            LetterU.SetActive(!isActive);
+            LetterV.SetActive(!isActive);
+            LetterW.SetActive(!isActive);
+            LetterX.SetActive(!isActive);
+            LetterY.SetActive(!isActive);
+            LetterZ.SetActive(!isActive);
             DEL.SetActive(!isActive);
         }
     }
